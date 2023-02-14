96d536611139749be6060cb13c06c0d929c91a8f
MichealFallon211436 <s00211436@atu.ie>
2023-02-14 15:22:01 +0000

    Angular Project Start

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'week2';
+  thingsToDo= [
+    'Learn javascript',
+    'Learn Angular',
+    'Learn Redux'
+  ]
+}
