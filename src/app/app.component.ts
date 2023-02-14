67dc12bbc6c1f353d7a382199e2183c55a66bf68
MichealFallon211436 <s00211436@atu.ie>
2023-02-14 15:44:05 +0000

    Finished Week2 Lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'week2';
+  name = 'Micheal Fallon S00211436'
+  title = 'To Do List';
   thingsToDo= [
     'Learn javascript',
     'Learn Angular',
     'Learn Redux'
   ]
+  thingsCompleted=[
+    'Complete Week 2 Work'
+  ];
+
+  TaskSummary():string {
+      return `${this.thingsToDo.length} to do / ${this.thingsCompleted.length} completed`
+  }
+  
+  constructor() {
+    setInterval(() => {
+      this.thingsToDo.push('make coffee');
+    },5000)
+  
+  }
 }
